fd8b4806338bfd500a6b49dc38aa2f5d33ad9e25
13PurpleHaze <kowalskiczeslaw399@gmail.com>
2025-08-28 17:31:32 +0500

    feature/tests add filter service tests

--- MangaApp/Services/Filter/FilterService.swift
+++ MangaApp/Services/Filter/FilterService.swift
@@ -13,25 +13,31 @@ protocol FilterServiceProtocol {
 }
 
 class FilterService: FilterServiceProtocol {
+    private let userDefaults: UserDefaults
+
+    init(userDefaults: UserDefaults = .standard) {
+        self.userDefaults = userDefaults
+    }
+
     func saveFields(filter: Filter) {
-        UserDefaults.standard.set(filter.contentRating.map { $0.rawValue }, forKey: "filter.contentRating")
-        UserDefaults.standard.set(filter.status.map { $0.rawValue }, forKey: "filter.status")
-        UserDefaults.standard.set(filter.order?.rawValue, forKey: "filter.order")
-        UserDefaults.standard.set(filter.search, forKey: "filter.search")
+        userDefaults.set(filter.contentRating.map { $0.rawValue }, forKey: "filter.contentRating")
+        userDefaults.set(filter.status.map { $0.rawValue }, forKey: "filter.status")
+        userDefaults.set(filter.order?.rawValue, forKey: "filter.order")
+        userDefaults.set(filter.search, forKey: "filter.search")
     }
 
     func fetchFields() -> Filter {
         var filter = Filter()
-        if let contentRating = UserDefaults.standard.object(forKey: "filter.contentRating") as? [String] {
+        if let contentRating = userDefaults.object(forKey: "filter.contentRating") as? [String] {
             filter.contentRating = contentRating.map { MangaContentRating(rawValue: $0) ?? .safe }
         }
-        if let status = UserDefaults.standard.object(forKey: "filter.status") as? [String] {
+        if let status = userDefaults.object(forKey: "filter.status") as? [String] {
             filter.status = status.map { MangaStatus(rawValue: $0) ?? .completed }
         }
-        if let order = UserDefaults.standard.object(forKey: "filter.order") as? String {
+        if let order = userDefaults.object(forKey: "filter.order") as? String {
             filter.order = MangaOrder(rawValue: order)
         }
-        if let search = UserDefaults.standard.object(forKey: "filter.search") as? String {
+        if let search = userDefaults.object(forKey: "filter.search") as? String {
             filter.search = search
         }
         return filter
